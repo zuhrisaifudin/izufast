f6b5299eafc9acb203277a7349b414396b075094
Zuhri <zuhri@khalidbasalamah.com>
2018-07-03 23:24:46 +0700

    add

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "https://github.com/zuhrisaifudin/izufast.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "home/deploy/izufast"
+set :deploy_to, "home/deploy/"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
