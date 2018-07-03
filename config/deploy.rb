9598ae8ec4899421bfd40fe8056b4e59161d41a2
Zuhri <zuhri@khalidbasalamah.com>
2018-07-03 23:19:33 +0700

    add

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, "https://github.com/zuhrisaifudin/izufast.git"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "home/deploy/izufast"
+set :deploy_to, "home/deploy/"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -27,7 +27,7 @@ set :deploy_to, "home/deploy/izufast"
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
-append :linked_files, "config/database.yml", "config/secrets.yml"
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 # Default value for default_env is {}
